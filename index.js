5ef9fecdaf30640f6eb237048c4c50b31d11d1fa
yuhenabc <yuhenabc@126.com>
2022-05-06 17:11:40 +0800

    export more funs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', { value: true });
+
 function _typeof(obj) {
   if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
     _typeof = function (obj) {
@@ -12,6 +14,97 @@ function _typeof(obj) {
   }
 
   return _typeof(obj);
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(source, true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(source).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
+function _slicedToArray(arr, i) {
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest();
+}
+
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+
+function _iterableToArrayLimit(arr, i) {
+  if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) {
+    return;
+  }
+
+  var _arr = [];
+  var _n = true;
+  var _d = false;
+  var _e = undefined;
+
+  try {
+    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+      _arr.push(_s.value);
+
+      if (i && _arr.length === i) break;
+    }
+  } catch (err) {
+    _d = true;
+    _e = err;
+  } finally {
+    try {
+      if (!_n && _i["return"] != null) _i["return"]();
+    } finally {
+      if (_d) throw _e;
+    }
+  }
+
+  return _arr;
+}
+
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance");
 }
 
 function capitalized(word) {
@@ -61,6 +154,42 @@ function toggleMutationFactory(setterKey, keyType) {
   }
 }
 
+function createMutationsAndActions(options) {
+  var setterList = options.setterList,
+      setterPrefix = options.setterPrefix,
+      toggleList = options.toggleList,
+      togglePrefix = options.togglePrefix;
+  var actions = {};
+  var mutations = {};
+  var mySetterPrefix = setterPrefix ? setterPrefix : 'set';
+  var myTogglePrefix = togglePrefix ? togglePrefix : 'toggle';
+
+  if (setterList) {
+    setterList.forEach(function (setterKey) {
+      var setterName = mySetterPrefix + capitalized(setterKey);
+      actions[setterName] = setterActionFactory(setterName);
+      mutations[setterName] = setterMutationFactory(setterKey);
+    });
+  }
+
+  if (toggleList) {
+    toggleList.forEach(function (toggleKey) {
+      var toggleName = myTogglePrefix + capitalized(toggleKey);
+      actions[toggleName] = toggleActionFactory(toggleName);
+      mutations[toggleName] = toggleMutationFactory(toggleKey);
+    });
+  }
+
+  return [actions, mutations];
+}
+function createActions(options) {
+  var r = createMutationsAndActions(options);
+  return r[0];
+}
+function createMutations(options) {
+  var r = createMutationsAndActions(options);
+  return r[1];
+}
 function vuexRichStore(storeObj) {
   if (_typeof(storeObj) !== 'object' || !storeObj['rich']) {
     return storeObj;
@@ -75,40 +204,33 @@ function vuexRichStore(storeObj) {
     delete storeObj['toggleList'];
     delete storeObj['togglePrefix'];
 
-    if (!storeObj.actions) {
-      storeObj.actions = {};
+    var _createMutationsAndAc = createMutationsAndActions({
+      setterList: setterList,
+      setterPrefix: setterPrefix,
+      toggleList: toggleList,
+      togglePrefix: togglePrefix
+    }),
+        _createMutationsAndAc2 = _slicedToArray(_createMutationsAndAc, 2),
+        mutations = _createMutationsAndAc2[0],
+        actions = _createMutationsAndAc2[1];
+
+    if (storeObj.actions) {
+      storeObj.actions = _objectSpread2({}, storeObj.actions, {}, actions);
+    } else {
+      storeObj.actions = actions;
     }
 
-    if (!storeObj.mutations) {
-      storeObj.mutations = {};
-    }
-
-    if (setterList) {
-      if (!setterPrefix) {
-        setterPrefix = 'set';
-      }
-
-      setterList.forEach(function (setterKey) {
-        var setterName = setterPrefix + capitalized(setterKey);
-        storeObj.actions[setterName] = setterActionFactory(setterName);
-        storeObj.mutations[setterName] = setterMutationFactory(setterKey);
-      });
-    }
-
-    if (toggleList) {
-      if (!togglePrefix) {
-        togglePrefix = 'toggle';
-      }
-
-      toggleList.forEach(function (toggleKey) {
-        var toggleName = togglePrefix + capitalized(toggleKey);
-        storeObj.actions[toggleName] = toggleActionFactory(toggleName);
-        storeObj.mutations[toggleName] = toggleMutationFactory(toggleKey);
-      });
+    if (storeObj.mutations) {
+      storeObj.mutations = _objectSpread2({}, storeObj.mutations, {}, mutations);
+    } else {
+      storeObj.mutations = mutations;
     }
 
     return storeObj;
   }
 }
 
-module.exports = vuexRichStore;
+exports.createActions = createActions;
+exports.createMutations = createMutations;
+exports.createMutationsAndActions = createMutationsAndActions;
+exports.default = vuexRichStore;
--- src/VuexRichStore.js
+++ src/VuexRichStore.js
@@ -6,46 +6,70 @@ import {
   toggleMutationFactory,
 } from './helpers'
 
+export function createMutationsAndActions(options) {
+  const { setterList, setterPrefix, toggleList, togglePrefix } = options
+  let actions = {}
+  let mutations = {}
+  const mySetterPrefix = setterPrefix ? setterPrefix : 'set'
+  const myTogglePrefix = togglePrefix ? togglePrefix : 'toggle'
+  if (setterList) {
+    setterList.forEach((setterKey) => {
+      const setterName = mySetterPrefix + capitalized(setterKey)
+      actions[setterName] = setterActionFactory(setterName)
+      mutations[setterName] = setterMutationFactory(setterKey)
+    })
+  }
+  if (toggleList) {
+    toggleList.forEach((toggleKey) => {
+      const toggleName = myTogglePrefix + capitalized(toggleKey)
+      actions[toggleName] = toggleActionFactory(toggleName)
+      mutations[toggleName] = toggleMutationFactory(toggleKey)
+    })
+  }
+  return [actions, mutations]
+}
+
+export function createActions(options) {
+  const r = createMutationsAndActions(options)
+  return r[0]
+}
+
+export function createMutations(options) {
+  const r = createMutationsAndActions(options)
+  return r[1]
+}
+
 export default function vuexRichStore(storeObj) {
   if (typeof storeObj !== 'object' || !storeObj['rich']) {
     return storeObj
   } else {
-    let {
-      setterList,
-      setterPrefix,
-      toggleList,
-      togglePrefix
-    } = storeObj
+    let { setterList, setterPrefix, toggleList, togglePrefix } = storeObj
     delete storeObj['rich']
     delete storeObj['setterList']
     delete storeObj['setterPrefix']
     delete storeObj['toggleList']
     delete storeObj['togglePrefix']
-    if (!storeObj.actions) {
-      storeObj.actions = {}
-    }
-    if (!storeObj.mutations) {
-      storeObj.mutations = {}
-    }
-    if (setterList) {
-      if (!setterPrefix) {
-        setterPrefix = 'set'
+    const [mutations, actions] = createMutationsAndActions({
+      setterList,
+      setterPrefix,
+      toggleList,
+      togglePrefix,
+    })
+    if (storeObj.actions) {
+      storeObj.actions = {
+        ...storeObj.actions,
+        ...actions,
       }
-      setterList.forEach(setterKey => {
-        const setterName = setterPrefix + capitalized(setterKey)
-        storeObj.actions[setterName] = setterActionFactory(setterName)
-        storeObj.mutations[setterName] = setterMutationFactory(setterKey)
-      })
+    } else {
+      storeObj.actions = actions
     }
-    if (toggleList) {
-      if (!togglePrefix) {
-        togglePrefix = 'toggle'
+    if (storeObj.mutations) {
+      storeObj.mutations = {
+        ...storeObj.mutations,
+        ...mutations,
       }
-      toggleList.forEach(toggleKey => {
-        const toggleName = togglePrefix + capitalized(toggleKey)
-        storeObj.actions[toggleName] = toggleActionFactory(toggleName)
-        storeObj.mutations[toggleName] = toggleMutationFactory(toggleKey)
-      })
+    } else {
+      storeObj.mutations = mutations
     }
     return storeObj
   }
